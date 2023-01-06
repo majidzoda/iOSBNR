be42c10dcf3fa2ba944e6d61070b0d04352cf4a8
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2023-01-06 18:41:41 -0500

    Chapter1: A simple iOS Application - Quiz - Building the Interface p. 45 - 48

---
+++ Quiz/Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Firdavsii Majidzoda on 1/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
