e7ed5beb7aa406f8aa3c440f762f78476c6d2a0f
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2023-01-06 19:17:07 -0500

    Chapter1: A simple iOS Application - Quiz - Creating the Model Layer p. 67

--- Quiz/Quiz/ViewController.swift
+++ Quiz/Quiz/ViewController.swift
@@ -4,6 +4,18 @@ class ViewController: UIViewController {
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
     
+    let questions: [String] = [
+        "What is 7+7?",
+        "What is the capital of Vemont?",
+        "What is cognac made from?"
+    ]
+    let answers: [String] = [
+        "14",
+        "Montpelier",
+        "Grapes"
+    ]
+    var currentQuestionIndex: Int = 0
+    
     @IBAction func showNextQuestion(_ sender: UIButton) {
         
     }
