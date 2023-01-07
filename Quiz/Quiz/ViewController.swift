4ea8b03a937bc6828c3dcb0e624db153bb9a327d
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2023-01-06 19:08:32 -0500

    Chapter1: A simple iOS Application - Quiz - Defining action methods p. 61

--- Quiz/Quiz/ViewController.swift
+++ Quiz/Quiz/ViewController.swift
@@ -3,5 +3,13 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        
+    }
 }
 
