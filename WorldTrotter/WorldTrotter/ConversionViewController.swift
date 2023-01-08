c9a994debe42305b71304365757becaa87f21fce
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2023-01-08 17:09:49 -0500

    Chapter4-ViewControllers: Bronze Challenge complited p. 194

--- WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -11,3 +11,18 @@ class ConversionViewController: UIViewController {
     }
 }
 
+/*
+Chapter4: View Controllers - Bronze Challenge p. 194: Another Tab
+Add a third tab to the WorldTrotter application. This tab should show the Quiz interface that you created
+in Chapter 1. A few notes to help you along:
+    In Chapter 1, the view controller’s name is ViewController. Consider renaming it to
+    QuizViewController.
+ 
+    You can drag the QuizViewController.swift file (or ViewController.swift, if you did not rename it)
+    from Finder into the WorldTrotter application in Xcode. When you do, make sure you check the Copy
+    items if needed checkbox to make a copy of the file, rather than moving it.
+    
+    You can copy the view controller scene from the storyboard in the Quiz project to the storyboard in
+    the WorldTrotter project.
+*/
+
