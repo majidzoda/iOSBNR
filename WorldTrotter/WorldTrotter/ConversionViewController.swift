f2faf367cff953caf7480a3fdd5c4a08a78b402d
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2023-01-08 15:52:23 -0500

    Chapter4-ViewControllers: Loaded and Appearing Views p. 185 - 190

---
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,13 @@
+
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("ConversionViewController loaded its view.")
+    }
+}
+
