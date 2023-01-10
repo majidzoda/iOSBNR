1de5987abf8151b30219e2c5ec235b35fbdf6d2f
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2023-01-10 12:02:55 -0500

    Chapter5: Text Input and Delegation - Implementing the temperature Conversion p. 239 - 241

--- WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -7,22 +7,44 @@ class ConversionViewController: UIViewController {
     @IBOutlet var celsiusLabel: UILabel!
     @IBOutlet var textField: UITextField!
     
+    var farenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    var celciusValue: Measurement<UnitTemperature>? {
+        if let farenheitValue = farenheitValue {
+            return farenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print("ConversionViewController loaded its view.")
+        
+        updateCelsiusLabel()
     }
     
     @IBAction func frenheitFieltEditingChanged(_ textField: UITextField) {
-        if let text = textField.text, !text.isEmpty{
-            celsiusLabel.text = textField.text
+        if let text = textField.text, let value = Double(text){
+            farenheitValue = Measurement(value: value, unit: .fahrenheit)
         } else {
-            celsiusLabel.text = "???"
+            farenheitValue = nil
         }
-        
     }
     
     @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
         textField.resignFirstResponder()
+    }
+    
+    func updateCelsiusLabel()  {
+        if let celciusValue = celciusValue {
+            celsiusLabel.text = "\(celciusValue.value)"
+        } else {
+            celsiusLabel.text = "???"
+        }
     }
 }
