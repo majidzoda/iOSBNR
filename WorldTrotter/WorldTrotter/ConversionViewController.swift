25be43505b97db37b86046e8a61d08b3af447853
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2023-01-10 12:09:07 -0500

    Chapter5: Text Input and Delegation - Number formatters p. 242 - 243

--- WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -20,6 +20,14 @@ class ConversionViewController: UIViewController {
         }
     }
     
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -42,9 +50,9 @@ class ConversionViewController: UIViewController {
     
     func updateCelsiusLabel()  {
         if let celciusValue = celciusValue {
-            celsiusLabel.text = "\(celciusValue.value)"
+            celsiusLabel.text = numberFormatter.string(from: NSNumber(value: celciusValue.value))
         } else {
             celsiusLabel.text = "???"
         }
-    }
+    }gi
 }
