04e652ef4863eb2d5a2a9eb9e620e9e67072ab51
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2023-01-10 11:41:09 -0500

    Chapter5: Text Input and Delegation - Responding to text field changes p. 231 - 234

--- WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -3,11 +3,22 @@
 import UIKit
 
 class ConversionViewController: UIViewController {
-
+    
+    @IBOutlet var celsiusLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print("ConversionViewController loaded its view.")
+    }
+    
+    @IBAction func frenheitFieltEditingChanged(_ textField: UITextField) {
+        if let text = textField.text, !text.isEmpty{
+            celsiusLabel.text = textField.text
+        } else {
+            celsiusLabel.text = "???"
+        }
+        
     }
 }
 
