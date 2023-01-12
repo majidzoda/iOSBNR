765ddfbefe4d9a5306f0e3422eee45176a285856
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2023-01-12 12:27:12 -0500

    Chapter8: Debugging - The Xcode Debugger: LLDB p. 311

--- Buggy/Buggy/ViewController.swift
+++ Buggy/Buggy/ViewController.swift
@@ -9,5 +9,20 @@ class ViewController: UIViewController {
     
     @IBAction func buttonTapped(_ sender: UIButton) {
         print("Method: \(#function) in file: \(#file) line: \(#line) called.")
+        
+        badMethod()
+    }
+    
+    func badMethod() {
+        let array = NSMutableArray()
+        
+        for i in 0..<10 {
+            array.insert(i, at: i)
+        }
+        
+        // Go one step too far emptying the array (notice the range change):
+        for _ in 0...10 {
+            array.removeObject(at: 0)
+        }
     }
 }
