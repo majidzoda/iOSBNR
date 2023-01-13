6172401483f33dd8dcba35330f56fe3612fec7cf
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2023-01-13 14:39:23 -0500

    Chapter9-UITableView-UITableViewControler: Moving Rows p. 370 - 373

--- LootLogger/LootLogger/ItemsViewController.swift
+++ LootLogger/LootLogger/ItemsViewController.swift
@@ -64,4 +64,10 @@ class ItemsViewController: UITableViewController {
             tableView.deleteRows(at: [indexPath], with: .automatic)
         }
     }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        // Update the model
+        itemStore.moveItem(from: sourceIndexPath.row, to: destinationIndexPath.row)
+        
+    }
 }
