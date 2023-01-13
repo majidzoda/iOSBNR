280a21edfbbb4644aa143d32447c31f41cd81b42
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2023-01-13 12:39:59 -0500

    Chapter9-UITableView-UITableViewControler: Implementing data source methods p. 346 - 347

--- LootLogger/LootLogger/ItemsViewController.swift
+++ LootLogger/LootLogger/ItemsViewController.swift
@@ -3,4 +3,7 @@ import UIKit
 class ItemsViewController: UITableViewController {
     var itemStore: ItemStore!
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemStore.allItems.count
+    }
 }
