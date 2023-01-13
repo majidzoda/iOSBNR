b2c58838cf21a2f11e2fb65a21aed794569f4c23
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2023-01-13 12:14:05 -0500

    Chapter9-UITableView-UITableViewControler: UITableView's Data Source p. 341 - 342

---
+++ LootLogger/ItemStore.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class ItemStore {
+    var allItems = [Item]()
+    
+    @discardableResult func createItem() -> Item {
+        let newItem = Item(random: true)
+        
+        allItems.append(newItem)
+        return newItem
+    }
+}
