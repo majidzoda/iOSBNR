59df8d42226fd26b9d7fbd5accb7d4fbeb3606bb
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2023-01-13 12:51:46 -0500

    Chapter9-UITableView-UITableViewControler: UITableViewCell p. 349 - 352

--- LootLogger/LootLogger/ItemsViewController.swift
+++ LootLogger/LootLogger/ItemsViewController.swift
@@ -6,4 +6,19 @@ class ItemsViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemStore.allItems.count
     }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Create an instance of UITableVieCell with default appereance
+        let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
+        
+        // Set the text on the cell with the description of the item
+        // that is at the nth index of item, where n = row this cell
+        // will appear in on the table view
+        let item = itemStore.allItems[indexPath.row]
+        
+        cell.textLabel?.text = item.name
+        cell.detailTextLabel?.text = "$\(item.valuesInDoller)"
+        
+        return cell
+    }
 }
