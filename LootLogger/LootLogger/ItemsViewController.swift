ed36d6479274b2e45158f20353d0f56b8d1f91ad
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2023-01-13 14:02:28 -0500

    Chapter9-UITableView-UITableViewControler: Editing Table Views p. 358 - 366

--- LootLogger/LootLogger/ItemsViewController.swift
+++ LootLogger/LootLogger/ItemsViewController.swift
@@ -3,14 +3,32 @@ import UIKit
 class ItemsViewController: UITableViewController {
     var itemStore: ItemStore!
     
+    @IBAction func addNewItem(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func toggleEditingMode(_ sender: UIButton){
+        // If you are currently in editing mode
+        if isEditing {
+            // Change text of button to inform userof state
+            sender.setTitle("Edit", for: .normal)
+            
+            // Turn off editing mode
+            setEditing(false, animated: true)
+        } else {
+            // Change text of button to inform user state
+            sender.setTitle("Done", for: .normal)
+            
+            // Enter editing mode
+            setEditing(true, animated: true)
+        }
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemStore.allItems.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // Create an instance of UITableVieCell with default appereance
-//        let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
-        
         // Create a new or recycled cell
         let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
         
