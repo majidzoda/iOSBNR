24c8b0e33c8b9cd69485851ecca9869474fda856
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2023-01-16 14:15:06 -0500

    Chapter10-SubclassingUITableViewCel - Dynamic Cell Heights p. 389 - 390

--- LootLogger/LootLogger/ItemsViewController.swift
+++ LootLogger/LootLogger/ItemsViewController.swift
@@ -6,7 +6,8 @@ class ItemsViewController: UITableViewController {
     override func viewDidLoad(){
         super.viewDidLoad()
         
-        tableView.rowHeight = 65
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 65
     }
     
     @IBAction func addNewItem(_ sender: UIButton) {
