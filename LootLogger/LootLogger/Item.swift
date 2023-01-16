4931dfd2e29d9bace22a4f28bcff8c2a96665e7f
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2023-01-16 14:59:30 -0500

    Chapter10-SubclassingUITableViewCel - Silver challenge complited p. 398

--- LootLogger/LootLogger/Item.swift
+++ LootLogger/LootLogger/Item.swift
@@ -15,7 +15,7 @@ class Item: Equatable {
     
     convenience init(random: Bool = false) {
         if random {
-            let adjectives = ["Fluffy", "Rusty", "Shiny"]
+            let adjectives = ["FluffyFluffyFluffyFluffyFluffyFluffyFluffy"]
             let nouns = ["Bear", "Spork", "Mac"]
             
             let randomAdjective = adjectives.randomElement()!
