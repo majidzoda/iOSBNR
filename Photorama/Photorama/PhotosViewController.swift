c07a922d75835d0a78be8a4eda21f95e984535d6
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2023-02-03 14:39:39 -0500

    Chapter20-WebServices: Building the URL - Sending the Request p. 680 - 685

--- Photorama/Photorama/PhotosViewController.swift
+++ Photorama/Photorama/PhotosViewController.swift
@@ -1,6 +1,13 @@
 import UIKit
 class PhotosViewController: UIViewController {
     @IBOutlet private var imageView: UIImageView!
+    var store: PhotoStore!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        store.fetchInterestingPhotos()
+    }
 }
 
 
--- Photorama/Photorama/SceneDelegate.swift
+++ Photorama/Photorama/SceneDelegate.swift
@@ -17,6 +17,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        let roorViewController = window!.rootViewController as! UINavigationController
+        let photosViewController = roorViewController.topViewController as! PhotosViewController
+        photosViewController.store = PhotoStore()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
