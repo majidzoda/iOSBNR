70bbfc2f8b6db610b16c69de1f01e1117cc23bbe
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2023-02-05 17:26:12 -0500

    Chapter21-CollectionView: Image caching p. 745 - 746

--- Photorama/Photorama/PhotoStore.swift
+++ Photorama/Photorama/PhotoStore.swift
@@ -6,6 +6,8 @@ enum PhotoError: Error {
 }
 
 class PhotoStore {
+    let imageStore = ImageStore()
+    
     private let session: URLSession = {
         let config = URLSessionConfiguration.default
         return URLSession(configuration: config)
@@ -37,6 +39,15 @@ class PhotoStore {
     
     func fetchImage(for photo: Photo,
                     completion: @escaping (Result<UIImage, Error>) -> Void) {
+        
+        let photoKey = photo.photoID
+        if let image = imageStore.image(forKey: photoKey) {
+            OperationQueue.main.addOperation {
+                completion(.success(image))
+            }
+            return
+        }
+        
         guard let photoURL = photo.remoteURL else {
             completion(.failure(PhotoError.missingImageURL))
             return
@@ -47,6 +58,9 @@ class PhotoStore {
             (data, response, error) in
             
             let result = self.processImageRequest(data: data, error: error)
+            if case let .success(image) = result {
+                self.imageStore.setImage(image, forKey: photoKey)
+            }
             OperationQueue.main.addOperation {
                 completion(result)
             }
