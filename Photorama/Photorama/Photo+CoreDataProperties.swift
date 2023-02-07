e9d610d5763c237502df1698bed37bb145fba13d
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2023-02-07 13:28:48 -0500

    Chapter22-CorData: NSManagedObject and subclasses p. 763 - 764

---
+++ Photorama/Photorama/Photo+CoreDataProperties.swift
@@ -0,0 +1,20 @@
+import Foundation
+import CoreData
+
+
+extension Photo {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Photo> {
+        return NSFetchRequest<Photo>(entityName: "Photo")
+    }
+
+    @NSManaged public var photoID: String?
+    @NSManaged public var title: String?
+    @NSManaged public var dateTaken: Date?
+    @NSManaged public var remoteURL: URL?
+
+}
+
+extension Photo : Identifiable {
+
+}
